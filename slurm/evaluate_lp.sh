431bdfaabc6bd46fb8dbe82accbbf1f1135b3e43
Roberto Barile <r.barile17@phd.uniba.it>
2025-10-16 15:44:05 +0200

    slurm

---
+++ slurm/evaluate_lp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-gpu=8
+#SBATCH --account=IscrC_MINA
+#SBATCH --partition=boost_usr_prod
+#SBATCH --qos=normal
+
+kg_name=$1
+kge_model_path=$2
+kge_config_path=$3
+
+python -m grainsack.operations evaluate-lp \
+    --kg_name $kg_name \
+    --kge_model_path $kge_model_path \
+    --kge_config_path $kge_config_path \
